f41c2883507ceff5e71d919a82728f2751c935c3
Tikhon <41113186+TikhonP@users.noreply.github.com>
2020-01-06 13:35:09 +0300

    Добавлен класс управления лампой yeelight

--- acssesorries.py
+++ acssesorries.py
@@ -1,4 +1,3 @@
-from yeelight import discover_bulbs
 from yeelight import Bulb
 from miio import PhilipsBulb
 
@@ -7,6 +6,8 @@ class philipsB:
     def __init__(self, ip, token):
         self.ip = str(ip)
         self.token = str(token)
+
+        self.bulb = PhilipsBulb(self.ip, self.token)
 
         self.cur_state = None
         self.cur_brightness = None
@@ -21,25 +22,93 @@ class philipsB:
             self.cur_state = state
             if self.cur_state == 'true':
                 try:
-                    self.bulb.turn_on()
-                except Exception:
+                    self.bulb.on()
+                except Exception as e:
                     self.bulb = PhilipsBulb(self.ip, self.token)
+                    self.bulb.on()
+                    print(e)
             else:
                 try:
-                    self.bulb.turn_off()
-                except Exception:
+                    self.bulb.off()
+                except Exception as e:
                     self.bulb = PhilipsBulb(self.ip, self.token)
+                    self.bulb.off()
+                    print(e)
                 continue
         elif self.cur_state == 'true':
             if brightness != self.cur_brightness:
                 self.cur_brightness = brightness
                 try:
                     self.bulb.set_brightness(self.cur_brightness)
-                except Exception:
+                except Exception as e:
                     self.bulb = PhilipsBulb(self.ip, self.token)
+                    self.bulb.set_brightness(self.cur_brightness)
+                    print(e)
             if color_temp != self.cur_color_temp:
                 self.cur_color_temp = color_temp
                 try:
                     self.bulb.set_color_temperature(self.cur_color_temp)
-                except Exception:
+                except Exception as e:
                     self.bulb = PhilipsBulb(self.ip, self.token)
+                    self.bulb.set_color_temperature(self.cur_color_temp)
+                    print(e)
+
+
+class yeelightB:
+    def __init__(self, ip, port):
+        self.ip = str(ip)
+        self.port = int(port)
+
+        self.bulb = Bulb(self.ip, self.port)
+
+        self.cur_state = None
+        self.cur_hsv = None
+        self.cur_color_temp = None
+
+    def update(self, state, hsv, color_temp):
+        state = str(state)
+        hsv = tuple(hsv)
+        color_temp = int(color_temp)
+
+        if state != self.cur_state:
+            self.cur_state = state
+            if self.cur_state == 'true':
+                try:
+                    self.bulb.turn_on()
+                except Exception as e:
+                    if e == 'Bulb closed the connection.':
+                        self.bulb = Bulb(self.ip, self.port)
+                        self.bulb.turn_on()
+                    else:
+                        raise e
+            else:
+                try:
+                    self.bulb.turn_off()
+                except Exception as e:
+                    if e == 'Bulb closed the connection.':
+                        self.bulb = Bulb(self.ip, self.port)
+                        self.bulb.turn_off()
+                    else:
+                        raise e
+                continue
+        elif self.cur_state == 'true':
+            if hsv != self.cur_hsv:
+                self.cur_hsv = hsv
+                try:
+                    self.bulb.set_hsv(hsv[0], hsv[1], hsv[2])
+                except Exception as e:
+                    if e == 'Bulb closed the connection.':
+                        self.bulb = Bulb(self.ip, self.port)
+                        self.bulb.set_hsv(hsv[0], hsv[1], hsv[2])
+                    else:
+                        raise e
+            if color_temp != self.cur_color_temp:
+                self.cur_color_temp = color_temp
+                try:
+                    self.bulb.set_color(self.cur_color_temp)
+                except Exception as e:
+                    if e == 'Bulb closed the connection.':
+                        self.bulb = Bulb(self.ip, self.port)
+                        self.bulb.set_color_temp(self.cur_color_temp)
+                    else:
+                        raise e
